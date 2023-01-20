71328dca535375f96065d591be9f12b942c8a53a
Loi Van Tran <datloiboi@gmail.com>
2023-01-20 18:16:52 -0500

    WIP remove non english

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       navigatorObservers: <NavigatorObserver>[observer],
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: MyHomePage(
         title: 'Semantics',
