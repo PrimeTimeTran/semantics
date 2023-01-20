98cee6078722171669a3f3adcca86da46eb22c4a
Loi Van Tran <datloiboi@gmail.com>
2023-01-20 15:59:30 -0500

    Add quotes from json

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:semantic/main.dart';
+import 'package:semantic/maincopy.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
